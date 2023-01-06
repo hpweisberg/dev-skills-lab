8d0b691e73ebb7a43e8b315d03e5d62a00d02a7d
hpweisberg <hpweisberg@gmail.com>
2023-01-05 17:39:37 -0800

    remove hardcodeded data for db

--- controllers/skills.js
+++ controllers/skills.js
@@ -1,22 +1,20 @@
-import { skills } from "../data/skills-data.js";
+// import { skills } from "../data/skills-data.js";
 import { Skill } from "../models/skills.js";
 
 function index(req, res){
+  //   res.render('skills/index', {
+  // skills: skills
+  Skill.find({})
+  .then(skills => {
     res.render('skills/index', {
-  skills: skills
+      skills: skills,
+    })
+  })
+  .catch(error => {
+    console.log(error)
+    res.redirect('/')
   })
 }
-  // skills.find({})
-  // .then(skills => {
-  //   res.render('skills/index', {
-  //     skills: skills,
-  //   })
-  // })
-//   .catch(error => {
-//     console.log(error)
-//     res.redirect('/')
-//   })
-// }
 
 function newSkill(req, res){
   res.render('skills/new')
@@ -53,5 +51,5 @@ export {
   newSkill as new,
   create,
   show,
-  skills,
+  // skills,
 }
